3625084bd2494b9536e676f2f3c002ee91ab89fc
Yash Aggarwal <yaggarwal@homeaway.com>
2018-10-04 21:09:56 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <p>👋 I'm in the process of building my website. For now, follow me on <a href="http://twitter.com/slushyash">Twitter</a>.</p>
+  </Layout>
+)
+
+export default IndexPage
